a50a79e578ab7b3d290ef3f19b5c5a92cf3fa69a
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-04 14:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+let currentAudio = null;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const sound = e.target.getAttribute("data-sound");
+
+    if (e.target.classList.contains("stop")) {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+    } else {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+
+      currentAudio = new Audio(`sounds/${sound}`);
+      currentAudio.play();
+    }
+  });
+});
