b409c6f9df96307049982905469aac30dd93ff6f
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-07 19:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const buttons = document.querySelectorAll('.btn');
 const stopBtn = document.querySelector('.stop');
-
 const audios = [];
+
 for (let i = 1; i <= 6; i++) {
-  // Using a silent base64-encoded audio clip (1 second of silence)
-  const audio = new Audio("data:audio/mp3;base64,//uQxAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8AAAACAAACcQCAAwACABAAZGF0YQAAAAA=");
+  const audio = document.getElementById(`audio${i}`);
+  audio.src = "data:audio/mp3;base64,//uQxAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8AAAACAAACcQCAAwACABAAZGF0YQAAAAA=";
   audios.push(audio);
 }
 
-// Stop all sounds
 function stopAll() {
-  audios.forEach(audio => {
-    audio.pause();
-    audio.currentTime = 0;
+  audios.forEach(a => {
+    a.pause();
+    a.currentTime = 0;
   });
 }
 
-
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
+buttons.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
     stopAll();
-    const audio = audios[index];
-    audio.play().catch(() => {}); 
-    console.log(`Playing sound ${index + 1}`); 
+    const audio = audios[i];
+    audio.play().catch(() => {});
   });
 });
 
-// Stop button
 stopBtn.addEventListener('click', stopAll);
